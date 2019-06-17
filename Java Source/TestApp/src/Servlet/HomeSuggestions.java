07b31ca330da17767a6c5eae6177c586efe9f8cb
Giannandrea <gn.vicidomini@gmail.com>
2019-06-17 15:20:03 +0200

    suggestions

--- Java Source/TestApp/src/Servlet/HomeSuggestions.java
+++ Java Source/TestApp/src/Servlet/HomeSuggestions.java
@@ -2,28 +2,32 @@ package Servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import DAOPackage.UserDAO;
-import JavaBeans.UserBean;
+import org.json.JSONArray;
+
+import DAOPackage.ProductDAO;
+import JavaBeans.ProductBean;
+
+
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class HomeSuggestions
  */
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/HomeSuggestions")
+public class HomeSuggestions extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginServlet() {
+    public HomeSuggestions() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,29 +37,16 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		UserBean usr = new UserBean();
-		usr.setUsername(request.getParameter("field_username"));
-		usr.setPassword(request.getParameter("field_password"));
-		usr.setName(request.getParameter("field_name"));
-		usr.setSurname(request.getParameter("field_surname"));
-		usr.setAddress(request.getParameter("field_address"));
-		usr.setCity(request.getParameter("field_city"));
-		usr.setCap(request.getParameter("field_cap"));
-		usr.setNation(request.getParameter("field_nation"));
-		usr.setPhone(request.getParameter("field_phone"));
-		usr.setEmail(request.getParameter("field_email"));
-		usr.setAdmin(false);
-		usr.setCanAccess(true);
-		UserDAO injector = new UserDAO();
+		JSONArray arr=new JSONArray();
+		ProductDAO prod=new ProductDAO();
+		ArrayList<ProductBean> all=null;
 		try {
-			injector.doSave(usr);
+			all=prod.doRetrieveAll();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.err.println("ERR: Cannot use the doSave method on the DAO object");
 		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp");
-		dispatcher.forward(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**

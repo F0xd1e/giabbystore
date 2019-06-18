289c2b03d707ff843f90f5e4dd97c83e4b41867b
Luis <bobo121@hotmail.it>
2019-06-18 18:59:06 +0200

    dosignup progress

--- Java Source/TestApp/src/Servlet/DoSignUp.java
+++ Java Source/TestApp/src/Servlet/DoSignUp.java
@@ -31,19 +31,36 @@ public class DoSignUp extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		UserDAO signupSetup = new UserDAO();
+		UserDAO signupSetup =  new UserDAO();
 		
-		String signuser =(String) request.getAttribute("username");
-		String signpassword = (String) request.getAttribute("password");
-		String signname = (String) request.getAttribute("name");
-		String signsurname = (String) request.getAttribute("surname");
-		String signnation = (String) request.getAttribute("nation");
-		String signcity = (String) request.getAttribute("city");
-		String signcap = (String) request.getAttribute("cap");
-		String signaddr = (String) request.getAttribute("address");
-		String signemail = (String) request.getAttribute("email");
-		String signphone = (String) request.getAttribute("phone");
+		String signuser     =  (String) request.getAttribute("username");
+		String signpassword =  (String) request.getAttribute("password");
+		String signname     =  (String) request.getAttribute("name");
+		String signsurname  =  (String) request.getAttribute("surname");
+		String signnation   =  (String) request.getAttribute("nation");
+		String signcity     =  (String) request.getAttribute("city");
+		String signcap      =  (String) request.getAttribute("cap");
+		String signaddr     =  (String) request.getAttribute("address");
+		String signemail    =  (String) request.getAttribute("email");
+		String signphone    =  (String) request.getAttribute("phone");
+		
+		boolean chuser= signuser.matches("/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/");
+		boolean chpassword = signpassword.matches("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,20}$/g");
+		boolean chsignname = signname.matches("/^[a-zA-Z]+$/");
+		boolean chsignsurname = signsurname.matches("/^[a-zA-Z]+$/");
+		boolean chsignnation = signnation.matches("/^[a-zA-Z]+$/");
+		boolean chsigncity = signcity.matches("/^[a-zA-Z]+$/");
+		boolean chsigncap = signcap.matches("/^[0-9]{5}/;");
+		boolean chsignaddr = signaddr.matches("/^[A-Za-z0-9 ,]+$/");
+		boolean chsignemail = signemail.matches("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/");
+		boolean chsignphone = signphone.matches("/^[0-9]/");
+
+		
+		// INCOMPLETE
+		
 		
 	}
+	
+
 
 }

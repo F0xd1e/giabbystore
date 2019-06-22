cb9f36efee797287dbd001b2bd32d479210a7999
Giannandrea <gn.vicidomini@gmail.com>
2019-06-22 22:49:04 +0200

    smol

--- Java Source/TestApp/src/Servlet/DoRetrieveProductBean.java
+++ Java Source/TestApp/src/Servlet/DoRetrieveProductBean.java
@@ -34,7 +34,7 @@ public class DoRetrieveProductBean extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println(request.getParameter("productId"));
+		
 		int productId=Integer.parseInt(request.getParameter("productId"));
 		ProductDAO pDao=new ProductDAO();
 		ProductBean prod=null;

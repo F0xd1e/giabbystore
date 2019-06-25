f2d463c60544e30a9feec87dbc9f838a8d7848d1
Giannandrea <gn.vicidomini@gmail.com>
2019-06-25 14:10:12 +0200

    updateProduct

---
+++ Java Source/TestApp/src/Servlet/DoUpdateProduct.java
@@ -0,0 +1,74 @@
+package Servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAOPackage.ProductDAO;
+import JavaBeans.ProductBean;
+
+/**
+ * Servlet implementation class DoUpdateProduct
+ */
+@WebServlet("/DoUpdateProduct")
+public class DoUpdateProduct extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoUpdateProduct() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int productCode=Integer.parseInt(request.getParameter("productCode"));
+		int availability=Integer.parseInt(request.getParameter("availability"));
+		
+		ProductDAO pDao=new ProductDAO();
+		ProductBean prod=null;
+		try {
+			prod=pDao.doRetrieveProductByID(productCode);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(prod==null) {
+			request.setAttribute("response", "No such element in the database.");
+			request.getRequestDispatcher("/WEB-INF/jsp/productpanel.jsp").forward(request, response);
+			return;
+		}
+		
+		prod.setAvailability(availability);
+		try {
+			pDao.doSaveOrUpdate(prod);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("response", "Availability successfully updated.");
+		request.getRequestDispatcher("/WEB-INF/jsp/productpanel.jsp").forward(request, response);
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
